63ed8b635821e9412fdcb69a9395aca5eca57d95
kaynareren <ismaleren321@gmail.com>
2021-11-28 23:59:07 +0300

    This is second commit

--- src/BaseJavaPractises/ExampleMain.java
+++ src/BaseJavaPractises/ExampleMain.java
@@ -3,5 +3,7 @@ package BaseJavaPractises;
 public class ExampleMain {
     public static void main(String[] args) {
         System.out.println("Hello git my name is eren kaynar");
+
+        System.out.println("Hello this is second commit");
     }
 }
