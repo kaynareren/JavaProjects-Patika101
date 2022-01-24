0a3b43d25f7722d055a7697f25a7474eb8938713
kaynareren <ismaleren321@gmail.com>
2022-01-25 01:53:00 +0300

    [BASIC-PRACTISES] I wrote new codes.YES

---
+++ src/SICAKLIK.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class SICAKLIK {
+    public static void main(String[] args) {
+
+        int heat;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("SICAKLIĞI GİRİNİZ : ");
+        heat = input.nextInt();
+
+        if (heat < 5) {
+            System.out.println("KAYAK YAPABILIRSINIZ");
+        } else if (heat <= 25) {
+            if (heat < 15){
+                System.out.println("SINEMAYA GIDEBILIRSINIZ");
+            }
+            if (heat >= 10) {
+                System.out.println("PIKNIGE GIDEBILIRSINIZ");
+            }
+            else {
+                System.out.println("YUZMEYE GIDEBILIRSINIZ.");
+            }
+        }
+
+
+
+
+
+
+    }
+}
